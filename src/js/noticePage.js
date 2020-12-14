f7b532ccc18ff60683faa6427fa5fbc4aeed8bdc
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2020-12-14 11:31:05 +0900

    Edit: 이미지파일 경로수정

--- src/js/noticePage.js
+++ src/js/noticePage.js
@@ -8,6 +8,7 @@ import { policyContents } from "./policyContents.js";
 
 const $policyMenu = document.querySelector(".policy-menu"); // 공지사항 페이지 네비게이션 바
 const $contents = document.querySelector(".contents");  // 페이지 메인 컨텐츠
+const menuTitles = ["이용약관", "개인정보처리방침", "운영정책", "공지사항"];
 
 for(let i=0; i<4; i++) {
     // 네비게이션 바의 각 메뉴(자식)들의 클릭 이벤트를 설정한다.
@@ -15,6 +16,7 @@ for(let i=0; i<4; i++) {
         $policyMenu.querySelector(".activate-menu").classList.remove("activate-menu");
         e.target.classList.add("activate-menu");
         
+        document.querySelector("title").innerText = `위뮤즈 | ${menuTitles[i]}`;
         $contents.innerHTML = policyContents[i];
         
         // 각각의 공지사항은 클릭 시 해당 글이 나와야함으로 클릭 이벤틀르 설정한다.
